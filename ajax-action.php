8a63075881a7179f5f1eca6e9fde5adb9dab80df
ваше имяIgorKhvostikgit config --global user.email ваша <imagination.dnb@gmail.com>
2017-09-22 00:07:05 +0300

    added new

--- ajax-action.php
+++ ajax-action.php
@@ -1,1 +1,48 @@
 <?php
+include_once ('connection.php');
+$action = $_REQUEST['action'];
+
+if($action === 'getCity'){
+    $result = [];
+
+    /** @var PDO $DBH */
+    $cities = $DBH->query('SELECT id,name FROM city order by name, id');
+    foreach($cities as $city ){
+        $result[$city['id']] = $city['name'];
+    }
+
+    echo json_encode($result);
+}elseif($action === 'getArea'){
+    $result = [];
+    $city_id = (int)$_REQUEST['city_id'];
+    /** @var PDO $DBH */
+    $areas = $DBH->query('SELECT id,name FROM city_area WHERE city_id="'.$city_id.'" order by name, id');
+    foreach($areas as $city ){
+        $result[$areas['id']] = $areas['name'];
+    }
+    echo json_encode($result);
+}elseif($action === 'getMyAddress'){
+
+    $addresses = $DBH->query('SELECT * ,main_info.id as id, city.name as city_name, city_area.name as area_name FROM main_info
+      INNER JOIN city ON main_info.city_id = city.id
+      INNER JOIN city_area ON main_info.area_id = city_area.id
+      order by main_info.id desc');
+    foreach($addresses as $address ) {
+        $items .= '<div class="item">
+										<h3>'.$address['name'].'</h3>
+										<p>'.$address['city_name'].','.$address['area_name'].' </p>
+										<div class="actbox">
+											<a href="#delete" data-id="'.$address['id'].'" class="bcross"></a>
+										</div>
+									</div>';
+    }
+
+    if($items) {
+        echo '<h2>My Addresses</h2>
+		   <div class="uo_adr_list">' . $items . '</div>';
+    }
+
+}elseif($action === "delete"){
+    $id = (int)$_REQUEST['id'];
+    $DBH->query("DELETE FROM main_info WHERE id='".$id."' LIMIT 1");
+}
--- js/ajax-action.js
+++ js/ajax-action.js
@@ -1,3 +1,54 @@
 $(function(){
-    alert('123');
+    var form = $('form[name="set_address_form"]');
+    var city = form.find('[name="city_id"]');
+    var area = form.find('[name="area_id"]');
+    var myAddressArea = $('#my_address_area');
+
+    load_city(city);
+
+    city.on("change",function(){
+        load_area($(this).val(),area);
+    });
+
+    get_my_address(myAddressArea);
+
+    $(document).on("click",'.bcross[data-id]', function(){
+        delete_address($(this).attr("data-id"));
+    });
 });
+
+function load_city($selector){
+    $.getJSON('/ajax-action.php',{'action':'getCity'},function(data){
+        changeSelect($selector,data)
+    });
+}
+
+function load_area(city_id,$selector){
+    $.getJSON('/ajax-action.php',{'action':'getArea','city_id':city_id},function(data){
+        changeSelect($selector,data)
+    });
+}
+
+function get_my_address($selector){
+    $.post('/ajax-action.php',{'action':'getMyAddress'},function (data) {
+        $selector.html(data);
+    });
+}
+
+function changeSelect($selector,data){
+    $selector.find("option:not([value='0'])").remove();
+    $.each(data,function(key, value){
+        var option = $('<option/>').attr('value',key).text(value);
+        $selector.append(option);
+    });
+}
+
+function delete_address(id){
+    if(confirm("Are you sure want to delete this address?")){
+        $.post('/ajax-action.php',{'action':'delete','id':id},function (data) {
+            console.log(data);
+            get_my_address($('#my_address_area'));
+        });
+    }
+}
+
